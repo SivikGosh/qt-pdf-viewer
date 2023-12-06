bc8d44b459e55df8c06f1a60339a0b083a0b61a3
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2023-12-06 23:01:10 +0300

    init

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ class PDFAppWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.load_file.clicked.connect(self.open_file)
-        self.pages_list = self.open_file()
+        self.page_list = []
+        self.page_count = 0
+        self.next_page.clicked.connect(lambda: self.show_next_page(self.page_count, self.page_list))
 
     def open_file(self):
         file_dir = QFileDialog.getOpenFileName(self, 'Open file', '', "PDF files (*.pdf)")[0]
@@ -24,10 +26,11 @@ class PDFAppWindow(QMainWindow, Ui_MainWindow):
             page.save(f'tmp/out{count}.png', 'PNG')
             pages_list.append(f'out{count}.png')
         self.view_page.setPixmap(QPixmap(f'tmp/{pages_list[0]}'))
-        return pages_list
+        self.page_count = len(pages_list)
+        self.page_list = pages_list
 
-    def next_page(self):
-        print(len(self.pages_list))
+    def show_next_page(self, i, lis):
+        self.view_page.setPixmap(QPixmap(f'tmp/{lis[1]}'))
 
         # reader = PdfReader(file_dir)
         # for i in range(len(reader.pages)):
