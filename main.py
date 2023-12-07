92a223ea655f5267f819d46892867ff237bbc111
SivikGosh <sivik@protonmail.com>
2023-12-07 11:49:54 +0300

    navigate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QLabel, QFileDialog, QMainWindow, QApplication
 from PyQt5.QtGui import QPixmap
 from PyPDF2 import PdfReader, PdfWriter
+import os
 
 from design import Ui_MainWindow
 from pdf2image import convert_from_path
@@ -15,38 +16,50 @@ class PDFAppWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.load_file.clicked.connect(self.open_file)
         self.page_list = []
-        self.page_count = 0
-        self.next_page.clicked.connect(lambda: self.show_next_page(self.page_count, self.page_list))
+        self.next_page.clicked.connect(lambda: self.show_next_page(self.page_list))
+        self.prev_page.clicked.connect(lambda: self.show_prev_page(self.page_list))
+        self.count = 0
 
     def open_file(self):
+        self.__clear_tmp_folder()
+        self.next_page.setEnabled(True)
+        self.prev_page.setEnabled(False)
+        self.count = 1
+
         file_dir = QFileDialog.getOpenFileName(self, 'Open file', '', "PDF files (*.pdf)")[0]
         pages_list = []
         pages = convert_from_path(file_dir, size=(800,))
-        for count, page in enumerate(pages):
-            page.save(f'tmp/out{count}.png', 'PNG')
-            pages_list.append(f'out{count}.png')
+
+        for num, page in enumerate(pages):
+            page.save(f'tmp/page_{num+1}.png', 'PNG')
+            pages_list.append(f'page_{num+1}.png')
+
         self.view_page.setPixmap(QPixmap(f'tmp/{pages_list[0]}'))
-        self.page_count = len(pages_list)
+        # self.page_count = len(pages_list)
         self.page_list = pages_list
 
-    def show_next_page(self, i, lis):
-        self.view_page.setPixmap(QPixmap(f'tmp/{lis[1]}'))
+    @staticmethod
+    def __clear_tmp_folder():
+        files = os.listdir('tmp')
+        for file in files:
+            os.remove(f'tmp/{file}')
 
-        # reader = PdfReader(file_dir)
-        # for i in range(len(reader.pages)):
-        #     writer = PdfWriter()
-        #     writer.add_page(reader.pages[i])
-        #     with open(f'tmp/{i+1}_page.pdf', 'wb') as f:
-        #         writer.write(f)
-        #     image = convert_from_path(f'tmp/{i+1}_page.pdf', fmt='png')
-        #     print(image.)
-        # print(pages_list)
-        # self.view_page.setPixmap(QPixmap(fname))
-        # images = convert_from_path('test.pdf', fmt='jpg')
-        # # images[0].save('test_!!!!.jpg', 'JPEG')
-        # # print(images[0].show())
-        # # self.view_page.setText(image)
-        # self.view_page.setPixmap(QPixmap(file_dir))
+    def show_next_page(self, lis):
+        if self.count < len(lis):
+            self.count += 1
+        self.view_page.setPixmap(QPixmap(f'tmp/{lis[self.count-1]}'))
+        if self.count > 1:
+            self.prev_page.setEnabled(True)
+        if self.count == len(lis):
+            self.next_page.setEnabled(False)
+
+    def show_prev_page(self, lis):
+        self.count -= 1
+        self.view_page.setPixmap(QPixmap(f'tmp/{lis[self.count-1]}'))
+        if self.count < len(lis):
+            self.next_page.setEnabled(True)
+        if self.count-1 < 1:
+            self.prev_page.setEnabled(False)
 
 
 def main():
